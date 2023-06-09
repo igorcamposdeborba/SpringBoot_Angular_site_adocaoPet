ecc7f718b7107c40a82a426ef763d652890eb4c3
igorcamposdeborba <igor_campos5@hotmail.com>
2023-06-08 21:26:03 -0300

    responsive front-end for mobile

--- frontend/src/app/components/elements/adoption-process-banner/adoption-process-banner.component.ts
+++ frontend/src/app/components/elements/adoption-process-banner/adoption-process-banner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-adoption-process-banner',
@@ -6,6 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./adoption-process-banner.component.css']
 })
 export class AdoptionProcessBannerComponent {
-
-
+  
+  @HostListener('window:resize', ['$event']) // listener da DOM para alteração do tamanho da tela
+  getImageSource(): string {
+    window.innerWidth;
+    if (window.innerWidth > 900) {
+      return 'assets/processo-adocao.jpg';
+      
+    } else if (window.innerWidth > 500) {
+      return 'assets/processo-adocao-tablet.jpg';
+      
+    } else {
+      return 'assets/processo-adocao-mobile.jpg';
+    }
+  }
 }
